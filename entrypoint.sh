275c67fcc6f0df1d223727889a60adad856cbed3
OpenMinded <vgerris@gmail.com>
2024-03-05 09:45:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Hello stranger!"
 time=$(date)
 export SOURCE_RPM="icinga2-2.13.5-1.fc36.icinga.src.rpm"
 export MOCK_CONFIG="rocky+epel-9-x86_64"
-/build-rpm.sh
+../build-rpm.sh
 echo "time=$time" >> $GITHUB_OUTPUT
